3c85c2701ca83fb5b174f252a72a397fef2ba395
ayocodingit <ayocodingit@gmail.com>
2024-10-09 15:02:21 +0700

    fix styling detail product

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -29,13 +29,14 @@ function DetailProduct() {
 
   return (
     <div className="flex flex-col bg-white h-screen relative">
-      <div className="w-full flex justify-center md:bg-gray-200">
+      <div className="w-full flex md:justify-center">
+      <div className="w-full md:w-1/2 flex justify-center md:bg-gray-200">
         <PhotoProvider className={`${!product.is_available && "grayscale"}`}>
           <PhotoView src={product.image}>
             <img
               src={product.image}
               alt="image product"
-              className={`max-h-[20rem] w-full md:w-52 relative object-contain rounded-t-lg hover: cursor-zoom-in object-top ${
+              className={`max-h-[20rem]  w-full md:w-96 relative object-contain rounded-t-lg hover: cursor-zoom-in object-top ${
                 !product.is_available && "grayscale"
               }`}
             />
@@ -43,23 +44,24 @@ function DetailProduct() {
         </PhotoProvider>
         <Link
           to={"/list"}
-          className="absolute start-5 top-5 bg-gray-50 p-2 rounded-md text-black"
+          className="absolute start-5 md:start-[26%] top-5 bg-gray-50 p-2 rounded-md text-black"
         >
           <BsChevronCompactLeft className="text-md" />
         </Link>
       </div>
+      </div>
       <div className="w-full flex md:justify-center">
-      <div className="md:w-1/2 flex ">
-        <div className="mt-2 p-2 flex gap-2">
-          <div className="flex flex-col">
-            <p className=" font-lg font-roboto capitalize">{product.name}</p>
-            <p className="text-orange-600 font-md font-serif ">
-              Rp{product.price} -{" "}
-              <span className="text-xs text-gray-800 ">{product.tag}</span>
-            </p>
+        <div className="md:w-1/2 flex ">
+          <div className="mt-2 p-2 flex gap-2">
+            <div className="flex flex-col">
+              <p className=" font-lg font-roboto capitalize">{product.name}</p>
+              <p className="text-orange-600 font-md font-serif ">
+                Rp{product.price} -{" "}
+                <span className="text-xs text-gray-800 ">{product.tag}</span>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <div className=" w-full absolute bottom-0 flex items-center md:justify-center">
         <div className="md:w-1/2 flex h-12 text-white font-xs w-full md:justify-center">
@@ -69,7 +71,12 @@ function DetailProduct() {
             </p>
           </div>
           <div className="bg-green-600 w-1/2 flex items-center justify-center font-serif">
-            <Link to={'/payment/' + product.username} className=" text-center text-white">Order</Link>
+            <Link
+              to={"/payment/" + product.username}
+              className=" text-center text-white"
+            >
+              Order
+            </Link>
           </div>
         </div>
       </div>
