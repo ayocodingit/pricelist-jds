19bfc2ec827616ab9c92307a3cbc5db62d5da26d
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-10 08:47:14 +0700

    Update DetailProduct.jsx

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -73,7 +73,7 @@ function DetailProduct() {
                   className="w-20 h-10 focus:outline-none border-b-2"
                   value={qty}
                   onChange={(e) => {
-                    setQty(parseInt(e.target.value));
+                    setQty(Number(e.target.value));
                     setTotal(product.price * qty);
                   }}
                 />
