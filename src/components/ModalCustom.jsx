9e5b9c0edea67eca71005004f535518a1655d34b
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-17 01:27:40 +0700

    Update ModalCustom.jsx

--- src/components/ModalCustom.jsx
+++ src/components/ModalCustom.jsx
@@ -5,7 +5,7 @@ import "./modalCustom.css";
 function ModalCustom({ modalIsOpen, closeModal, children }) {
   const customStyles = {
     content: {
-      top: "100%",
+      top: "90%",
       left: "50%",
       right: "auto",
       bottom: "auto",
