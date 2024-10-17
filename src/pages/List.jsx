5d01b66e640bf8225846f20e603cae6c64db220f
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-17 15:43:47 +0700

    Update List.jsx

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -85,7 +85,7 @@ function List() {
             >
               <BiUserCircle className="text-3xl text-white hover:cursor-pointer" onClick={() => setShowProfile((prev) => !prev)}/>
               {getCustomer() && showProfile && (
-                <div className=" absolute -bottom-10 right-0 w-32 h-32 z-20 bg-white shadow-md text-black p-2 rounded-md ">
+                <div className=" absolute top-10 right-0 w-32 h-32 z-20 bg-white shadow-md text-black p-2 rounded-md ">
                   <div className="flex items-center justify-between gap-2">
                     <p className="text-xs">Hai, {getCustomer()?.customer} </p>
                     <BiPencil className="hover:cursor-pointer" onClick={() => setIsModalCustomer((prev) => !prev)} />
