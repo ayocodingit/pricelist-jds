0cd223479b57e516c1d4812cb7dedc6fe6409be7
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-18 21:50:38 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/route.jsx";
 import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
