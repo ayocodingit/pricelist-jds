24f5e32f026681fe5e4845c2ff1bce88d84ddd0b
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-19 12:11:51 +0700

    Update Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,7 +5,7 @@ function Homepage() {
   return (
     <div className="h-[calc(100dvh)] flex flex-col justify-center items-center gap-10 bg-gray-50">
       <div className="flex flex-col items-center   gap-2">
-        <h1 className="text-4xl font-serif tracking-widest">Price list </h1>
+        <h1 className="text-4xl font-serif tracking-widest">Welcome to JDS Mart </h1>
         <p className="text-md tracking-widest">
           Small and Medium enterprises (SMEs)
         </p>
@@ -28,7 +28,7 @@ function Homepage() {
           <div className="flex-grow border-t border-black border-2"></div>
         </div>
         <Link
-          className="w-40 h-8 text-center flex justify-center items-center rounded-lg border-2 text-white hover:bg-opacity-90 bg-[#5D9F5D] "
+          className="w-40 h-8 text-center flex justify-center items-center rounded-lg border-2 text-white hover:bg-opacity-90 bg-primary "
           to={"list"}
         >
           Start
