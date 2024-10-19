b190cd23e8080e72ad18f511edc39917d360f575
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-20 00:21:43 +0700

    Update List.jsx

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -112,7 +112,7 @@ function List() {
         {/* Product  */}
         <div className="px-2 py-2 flex flex-col gap-3 bg-white justify-center">
           {products.length > 0 && (
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 h-[calc(73dvh)] p-2 overflow-auto">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 h-[calc(75dvh)] p-2 overflow-auto">
               {products.map((product, index) => {
                 return (
                   <ProductList product={product} key={index}></ProductList>
@@ -121,7 +121,7 @@ function List() {
             </div>
           )}
           {products.length === 0 && (
-            <div className="capitalize justify-center flex items-center h-[calc(73dvh)]">
+            <div className="capitalize justify-center flex items-center h-[calc(50dvh)]">
               Product is Not Found
             </div>
           )}
