2872134343422f5c7bd1f0ce4e04bc8d0f0f3be2
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-19 16:43:56 +0700

    Update DetailProduct.jsx

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -123,7 +123,7 @@ Hatur nuhun~ ✨
                   onClick={() => navigate("/cart")}
                 >
                   <CiShoppingCart className="text-3xl " />
-                  <p className="absolute rounded-full top-2 right-2 outline-black outline-1 outline-double bg-white text-xs w-4 flex justify-center">
+                  <p className="absolute rounded-full top-1 right-1 outline-black outline-1 outline-double bg-white text-xs w-4 flex justify-center">
                     {totalCart}
                   </p>
                 </div>
