727e17c2b9c487f5c5a4204bee034187f470c7ce
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-19 16:12:15 +0700

    Update SuccessOrder.jsx

--- src/pages/SuccessOrder.jsx
+++ src/pages/SuccessOrder.jsx
@@ -20,7 +20,7 @@ function SuccessOrder() {
           </p>
           <Link
             to={"/list"}
-            className="text-white bg-primary rounded-lg shadow-lg p-2 py-4"
+            className="text-white bg-primary rounded-lg shadow-lg p-2 py-5"
           >
             Homepage
           </Link>
