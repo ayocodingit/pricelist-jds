d3d7fc68efdbd1cf719d4b2cc620059aa43485e9
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-20 14:06:23 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className="text-sm flex gap-1">
-        For Manage Your Product Message 
-        <a href="https://t.me/firmanalamsyah580" className="underline">
+        For Register New Product you can Join
+        <a href="https://t.me/+zVf_gRBkoixlNTg1?text=halo min, aku ingin join mendaftarkan produk dong" className="underline">
           Here
         </a>
     </div>
