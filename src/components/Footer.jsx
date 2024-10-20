9e12d305bbce6b4fd2774f61820c72d5a912198b
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-20 14:08:35 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <div className="text-sm flex gap-1">
         For Register New Product you can Join
-        <a href="https://t.me/+zVf_gRBkoixlNTg1?text=halo min, aku ingin join mendaftarkan produk dong" className="underline">
+        <a href="https://t.me/+zVf_gRBkoixlNTg1" className="underline">
           Here
         </a>
     </div>
