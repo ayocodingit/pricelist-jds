d0922820130a0557e21d6cb20361f2de98ee532b
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-21 22:38:19 +0700

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <div className="text-md flex gap-1">
-      Tertarik Berjualan! Join
+      Segera Daftarkan Produkmu! Join
       <a href="https://t.me/+zVf_gRBkoixlNTg1" className="underline">
         Disini
       </a>
