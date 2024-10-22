f741d796f74d7884bc328b35c7d81742867c8753
ayocodingit <ayocodingit@gmail.com>
2024-10-22 08:21:48 +0700

    add feedback form

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -116,7 +116,7 @@ function List() {
         {/* Product  */}
         <div className="px-2 py-2 flex flex-col gap-3 bg-white justify-center">
           {products.length > 0 && (
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 h-[calc(75dvh)] p-2 overflow-auto">
+            <div className="grid grid-cols-2 md:grid-cols-4 gap-2 h-[calc(73dvh)] p-2 overflow-auto">
               {products.map((product, index) => {
                 return (
                   <ProductList product={product} key={index}></ProductList>
--- src/pages/SuccessOrder.jsx
+++ src/pages/SuccessOrder.jsx
@@ -9,21 +9,28 @@ function SuccessOrder() {
   }, []);
 
   return (
-    <div className="bg-white h-[calc(100dvh)] flex justify-center items-center">
+    <div className="bg-white h-[calc(100dvh)] flex justify-center items-center text-center">
       <div className="w-full md:w-1/2 h-full flex flex-col justify-center">
         <img src="/order-success.webp" className="w-full" alt="" />
         <div className="min-h-[calc(50dvh)] text-sm flex flex-col items-center justify-center gap-4 bg-white text-md">
           <AiOutlineCheckCircle className="text-5xl text-primary" />
           <p className="text-lg font-bold">Pemesanan Produk Berhasil!</p>
-          <p className="text-center">
-            Pesanan Anda sudah disampaikan kepada penjual yang bersangkutan
-          </p>
-          <Link
-            to={"/list"}
-            className="text-white bg-primary rounded-lg shadow-lg p-2 my-5 px-5"
-          >
-            Kembali Belanja
-          </Link>
+          <p>Pesanan Anda sudah disampaikan kepada penjual yang bersangkutan</p>
+          <div className="flex gap-2 items-center">
+            <Link
+              to={"/list"}
+              className="text-white bg-primary rounded-lg shadow-lg p-2 my-5 px-5"
+            >
+              Kembali Belanja
+            </Link>
+            <a
+              href={import.meta.env.VITE_URL_FEEDBACK}
+              target="_blank"
+              className="bg-orange-500 p-2 rounded-lg text-white shadow-md"
+            >
+              Ada Feedback?
+            </a>
+          </div>
         </div>
       </div>
     </div>
