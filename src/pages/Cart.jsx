3dee1b917d53ac29ce75d5fd0409d2a9b1397cc6
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-26 21:29:54 +0700

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -111,7 +111,7 @@ function Cart() {
           >
             {carts.map((cart, index) => {
               return (
-                <div key={index} className="border-b-4">
+                <div key={index} className="border-b-8">
                   <div className="text-black py-2  flex gap-1 items-center font-bold">
                     <input
                       type="checkbox"
