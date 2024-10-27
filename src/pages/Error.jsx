70ece63644efc82eb8f6f5b76ed5671bb1b1c00d
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-10-27 13:08:50 +0700

    Update Error.jsx

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ function Error() {
   const error = useRouteError();
   console.error(error);
   return (
-    <div className="flex flex-col text-center gap-2 items-center justify-center h-[calc(100dvh)] text-2xl">
+    <div className="flex flex-col text-center gap-2 items-center justify-center h-[calc(100dvh)] text-lg">
       <img src="/not-found.png" alt="not found" width={180} />
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
