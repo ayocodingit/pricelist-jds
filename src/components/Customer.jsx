0d82e9f3513243a2855e38c9643eab76c4eab831
ayocodingit <ayocodingit@gmail.com>
2024-11-05 22:30:23 +0700

    add page profile

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import {
   checkCompleteCustomer,
   getCustomer,
@@ -11,7 +11,14 @@ import Loading from "./Loading";
 
 function Customer() {
   const { customer, telegram } = getCustomer();
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000);
+  }, [isLoading]);
 
   const formik = useFormik({
     initialValues: {
@@ -19,15 +26,15 @@ function Customer() {
       telegram: telegram ?? "",
     },
     onSubmit: (values) => {
-      setIsLoading(true);
+      setIsLoadingSubmit(true);
       storeCustomer(values);
       alert(
         "success",
         `Profil berhasil ${checkCompleteCustomer() ? "diubah" : "disimpan"}`
       );
       setTimeout(() => {
-        setIsLoading(false);
-        location.reload();
+        setIsLoadingSubmit(false);
+        setIsLoading(true);
       }, 2000);
     },
     validationSchema: yup.object().shape({
@@ -53,52 +60,61 @@ function Customer() {
       transition: Flip,
     });
   };
+
   return (
     <form
       className="flex flex-col items-center  w-full   p-5"
       onSubmit={formik.handleSubmit}
     >
-      <div className="bg-white w-full p-4 rounded-lg  shadow-lg flex flex-col items-center">
+      <div className="bg-white w-full min-h-96 p-10 rounded-lg  shadow-lg flex flex-col items-center">
         <img src="/user.png" className="w-32 object-contain" alt="logo user" />
-
-        <div className="flex items-center w-full mt-5">
-          <label className="w-1/3">Nama Lengkap</label>
-          <input
-            placeholder="Nama Lengkap - Divisi"
-            className="border-b-[1px] focus:border-primary w-full focus:outline-none bg-transparent p-2"
-            {...formik.getFieldProps("customer")}
-          />
-        </div>
-        <div className="w-full flex mt-1">
-          <div className="w-1/3">&nbsp;</div>
-          <span className="w-full first-letter:capitalize italic text-xs text-red-500">
-            {formik.errors.customer}
-          </span>
-        </div>
-        <div className="flex items-center w-full">
-          <label className="w-1/3">Akun Telegram</label>
-          <input
-            placeholder="@aseptea"
-            className="border-b-[1px] focus:border-primary w-full focus:outline-none bg-transparent p-2"
-            {...formik.getFieldProps("telegram")}
-          />
-        </div>
-        <div className="w-full flex mt-1">
-          <div className="w-1/3">&nbsp;</div>
-          <span className="w-full first-letter:capitalize italic text-xs text-red-500">
-            {formik.errors.telegram}
-          </span>
-        </div>
-        <button
-          type="submit"
-          className={`bg-primary text-white rounded-md h-8 md:w-1/2 w-full flex gap-3 justify-center items-center mt-8 hover:cursor-pointer ${
-            (isLoading || !formik.isValid) && "opacity-60"
-          }`}
-          disabled={formik.isSubmitting || !formik.isValid}
-        >
-          {checkCompleteCustomer() ? "Ubah" : "Simpan"}
-          <Loading isLoading={isLoading} size={15} color="#fff"></Loading>
-        </button>
+        {!isLoading && (
+          <>
+            <div className="flex items-center w-full mt-5">
+              <label className="w-1/3">Nama Lengkap</label>
+              <input
+                placeholder="Nama Lengkap - Divisi"
+                className="border-b-[1px] focus:border-primary w-full focus:outline-none bg-transparent p-2"
+                {...formik.getFieldProps("customer")}
+              />
+            </div>
+            <div className="w-full flex mt-1">
+              <div className="w-1/3">&nbsp;</div>
+              <span className="w-full first-letter:capitalize italic text-xs text-red-500">
+                {formik.errors.customer}
+              </span>
+            </div>
+            <div className="flex items-center w-full">
+              <label className="w-1/3">Akun Telegram</label>
+              <input
+                placeholder="@aseptea"
+                className="border-b-[1px] focus:border-primary w-full focus:outline-none bg-transparent p-2"
+                {...formik.getFieldProps("telegram")}
+              />
+            </div>
+            <div className="w-full flex mt-1">
+              <div className="w-1/3">&nbsp;</div>
+              <span className="w-full first-letter:capitalize italic text-xs text-red-500">
+                {formik.errors.telegram}
+              </span>
+            </div>
+            <button
+              type="submit"
+              className={`bg-primary text-white rounded-md h-8 md:w-1/2 w-full flex gap-3 justify-center items-center mt-8 hover:cursor-pointer ${
+                (isLoadingSubmit || !formik.isValid) && "opacity-60"
+              }`}
+              disabled={isLoadingSubmit || !formik.isValid}
+            >
+              {checkCompleteCustomer() ? "Ubah" : "Simpan"}
+              <Loading
+                isLoading={isLoadingSubmit}
+                size={15}
+                color="#fff"
+              ></Loading>
+            </button>
+          </>
+        )}
+        {isLoading && <div className="flex items-center w-full justify-center h-full"><Loading /></div>}
       </div>
     </form>
   );
