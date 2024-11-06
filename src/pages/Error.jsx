9ad7c23d574da00bd83be4c0d2df4684fa9e5062
ayocodingit <ayocodingit@gmail.com>
2024-11-06 10:20:13 +0700

    fix font size on device laptop

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -5,7 +5,7 @@ function Error() {
   const error = useRouteError();
   console.error(error);
   return (
-    <div className="flex flex-col text-center gap-2 items-center justify-center h-[calc(100dvh)] text-sm  md:text-lg">
+    <div className="flex flex-col text-center gap-2 items-center justify-center h-[calc(100dvh)] text-sm  md:text-md">
       <img src="/not-found.png" alt="not found" width={180} />
       <p>Sorry, an unexpected error has occurred.</p>
     </div>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,7 @@ import Menu from '../components/Menu'
 
 function Profile() {
   return (
-    <div className='h-[calc(100dvh)] bg-gray-50 text-sm md:text-lg flex md:justify-center'>
+    <div className='h-[calc(100dvh)] bg-gray-50 text-sm md:text-md flex md:justify-center'>
         <div className='flex w-full md:w-1/2 items-center mt-32 flex-col'>
             <Customer></Customer>
         </div>
