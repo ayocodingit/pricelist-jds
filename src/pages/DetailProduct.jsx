8f8c0d82415f07f867f359010f8091b64e6febc5
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-11-09 22:19:46 +0700

    Update DetailProduct.jsx

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -163,7 +163,7 @@ Hatur nuhun~ ✨`}
             className={`w-full h-[20rem] md:h-[calc(60dvh)] hover:cursor-zoom-in rounded-md object-contain`}
           />
         </div>
-        <div className="p-3 flex flex-col gap-2 md:max-h-[calc(99dvh)] h-[calc(80dvh)] md:justify-center md:p-10 bg-white md:w-1/2 md:shadow-md md:rounded-md md:overflow-auto">
+        <div className="p-3 flex flex-col gap-2 md:max-h-[calc(99dvh)] h-[calc(50dvh)] md:justify-center md:p-10 bg-white md:w-1/2 md:shadow-md md:rounded-md md:overflow-auto">
           <div className="flex justify-between">
             <div>
               <p
