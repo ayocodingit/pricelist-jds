681a99dfffc024f81f2254cc7c8d244bc4ef7ac1
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-11-09 08:14:56 +0700

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -38,7 +38,7 @@ function ProductList({ product }) {
           <img
             src={product.image}
             alt="image product"
-            className={`w-full h-32 object-contain`}
+            className={`w-full h-32 object-contain p-2 `}
             loading="lazy"
           />
           {isStockEmpty && (
