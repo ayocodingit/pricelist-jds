e9fce870695f5d50e19f1a5f415d9d8956a7f896
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-11-17 11:27:08 +0700

    Update DetailProduct.jsx

--- src/pages/DetailProduct.jsx
+++ src/pages/DetailProduct.jsx
@@ -292,7 +292,7 @@ Hatur nuhun~ ✨`}
                   {product.variants?.map((variant, index) => (
                     <Button
                       key={index}
-variant="bordered"
+variant="ghost"
                       className={`rounded-md capitalize ${
                         formik.values.variant == variant
                           ? "bg-primary text-white"
