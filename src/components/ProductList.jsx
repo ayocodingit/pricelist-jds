222ca30ff5a53a27ebfb521de315d1d15f84b884
Firman Ayo Coding <41193120+ayocodingit@users.noreply.github.com>
2024-11-19 19:34:54 +0700

    Update ProductList.jsx

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -22,7 +22,7 @@ function ProductList({ product }) {
 
   return (
     <Link
-      className="border-[1px] border-primary rounded-md flex bg-white hover:cursor-pointer h-48 md:h-60 relative w-full"
+      className=" rounded-md flex bg-white hover:cursor-pointer h-48 md:h-60 relative w-full"
       to={"/list/" + product.id}
     >
       <div className="flex flex-col justify-center w-full">
