dc90327261b03fe26b3f54c9c109d81dc5d25222
ayocodingit <ayocodingit@gmail.com>
2024-12-13 15:26:13 +0700

    add product from mike

--- src/utils/promo.js
+++ src/utils/promo.js
@@ -1,7 +1,15 @@
-const DealDiscount = (qty, multiple = 2, discountPerPair = 1000) => {
-  return Math.floor(qty / multiple) * discountPerPair;
+const DealDiscount = (qty, multiple = 2, discount = 1000) => {
+  return Math.floor(qty / multiple) * discount;
 };
+
+function ThresholdDiscount(qty, min, discount) {
+  let voucher = 0;
+  if (qty >= min) voucher = discount * qty;
+
+  return voucher;
+}
 
 export default {
   DealDiscount,
+  ThresholdDiscount,
 };
